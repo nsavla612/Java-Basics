4578b9448bf0d9b17e007b2a78999d0a5a2783b1
Nishit Savla <nsavla6121991@gmail.com>
2024-05-21 10:03:25 -0700

    Update Main.java

--- FileFinder/Main.java
+++ FileFinder/Main.java
@@ -2,20 +2,52 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-        File f1 = new File("/home/local/savlnish/test.java" , 500, false);
-        File f2 = new File("/home/local/savlnish/test.xml" , 200 , false);
-        File f3 = new File("/home/local/savlnish/test2.java" , 100, false);
+        File f1 = new File.Builder().
+                FilePath("/home/local/savlnish/test.java").
+                size(500).
+                isDirectory(false).
+                build();
 
-        File f5 = new File("/home/local/savlnish/folder/foldertest5.java" , 200, false);
-        File f6 = new File("/home/local/savlnish/folder/foldertest6.xml" , 400, false);
-        File f7 = new File("/home/local/savlnish/folder/foldertest.dir", 0 ,true);
-        f7.setFiles( f5,f6);
+        File f2 = new File.Builder().
+                FilePath("/home/local/savlnish/test.xml").
+                size(200).
+                isDirectory(false).
+                build();
 
-        File f = new File("/home/local/savlnish/test.dir", 0 ,true);
-        f.setFiles( f1,f2,f3, f7);
+        File f3 = new File.Builder().
+                FilePath("/home/local/savlnish/test2.java").
+                size(100).
+                isDirectory(false).
+                build();
+
+        File f5 = new File.Builder().
+                FilePath("/home/local/savlnish/folder/foldertest5.java").
+                size(100).
+                isDirectory(false).
+                build();
+
+        File f6 = new File.Builder().
+                FilePath("/home/local/savlnish/folder/foldertest6.xml").
+                size(400).
+                isDirectory(false).
+                build();
+
+        File f7 = new File.Builder().
+                FilePath("/home/local/savlnish/folder/foldertest.dir").
+                size(0).
+                isDirectory(true).
+                addFiles(f5,f6).
+                build();
+
+        File f = new File.Builder().
+                FilePath("/home/local/savlnish/test.dir").
+                size(0).
+                isDirectory(true).
+                addFiles(f1,f2,f3,f7).
+                build();
 
         FileRule rule = new FileRuleOr(
-                new FileRuleExtension("xml"),
+                new FileRuleExtension("java"),
                 new FileRuleAnd(
                         new FileRuleExtension("java"),
                         new FileRuleSize(200, FileRuleSize.FileRuleSizeOp.GTE)
@@ -23,115 +55,5 @@ public class Main {
         );
 
         FindFile.find(f, rule);
-    }
-}
-
-
-// FileRule.java
- interface FileRule {
-    public boolean match(File f);
-}
-
-
- class FileRuleAnd implements  FileRule {
-
-    private final List<FileRule> andRules;
-
-    public FileRuleAnd(FileRule... andRules) {
-        this.andRules = Arrays.asList(andRules);
-    }
-
-    @Override
-    public boolean match(File f) {
-        for(FileRule rule : andRules)
-        {
-            if(!rule.match(f)) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
- class FileRuleOr implements  FileRule {
-
-    private final List<FileRule> orRules;
-
-    public FileRuleOr(FileRule... orRules) {
-        this.orRules = Arrays.asList(orRules);
-    }
-
-    @Override
-    public boolean match(File f) {
-        for(FileRule rule : orRules)
-        {
-            if(rule.match(f)) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
- class FileRuleExtension implements FileRule {
-    final private String ext;
-    public FileRuleExtension(String ext)
-    {
-        this.ext = ext;
-    }
-
-    @Override
-    public boolean match(File f) {
-        return f.getFilename().endsWith("." + ext);
-    }
-}
-
- class FileRuleSize implements  FileRule {
-
-    enum FileRuleSizeOp {
-        LT,
-        LTE,
-        EQ,
-        GT,
-        GTE
-    }
-    private final int size;
-    private final FileRuleSizeOp op;
-
-    public FileRuleSize(int filesize, FileRuleSizeOp op) {
-        this.size = filesize;
-        this.op = op;
-    }
-
-    @Override
-    public boolean match(File f) {
-       switch (op) {
-           case LT: return f.getFilesize() < size;
-           case LTE:return f.getFilesize() <= size;
-           case EQ:return f.getFilesize() == size;
-           case GT:return f.getFilesize() > size;
-           case GTE:return f.getFilesize() >= size;
-       }
-       return false;
-    }
-}
-
- class FindFile {
-    public static void find( File baseDir, FileRule rule) {
-        if(baseDir.isDirectory()) {
-            for(File f : baseDir.getFiles())
-            {
-                if(f.isDirectory())
-                {
-                   find(f, rule);
-                }
-                else {
-               //     System.out.println(f.getFilename() + " about to start with rule- " + rule.toString());
-                    if (rule.match(f)) {
-                        System.out.println(f.getFilename() + " matches");
-                    }
-                }
-            }
-        }
     }
 }
